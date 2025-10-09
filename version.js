7b9e7faf111a39fa1abca8aacb921043cd3e43c2
operRehon <dwazuba@yandex.ru>
2025-10-09 14:32:51 +0500

    Update version.js

--- version.js
+++ version.js
@@ -1,6 +1,6 @@
 window.APP_CONFIG = {
     VERSION: '2.1.0',
     BUILD_DATE: '2025-10-09',
-    GIT_COMMIT: 'a1b2c3d',
+    GIT_COMMIT: '8dcc03645713ae7a420854cda7538d30f9745d00',
     AUTO_LOAD: true
 };
